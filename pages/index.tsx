16c2f166dc7b3fc6b67d8e8d1b31638f9f8c65c9
TDC demo <demo@thedevelopercompany.com>
2022-05-21 12:18:07 +0530

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
-  return <div></div>
+  return (
+    <div>
+      <Link href="/blogs">
+        <a className="text-blue-700 text-xl">Go to blogs</a>
+      </Link>
+    </div>
+  )
 }
 
 export default Home
